a151af47c4de487e81c6262975ae25f628ac4f91
ivansheshin <ivashesh@gmail.com>
2023-06-11 20:30:20 +0400

    added pages

---
+++ src/pages/FilmsPage.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import FilmsList from '@/features/FilmsList/FilmsList.vue'
+</script>
+
+<template>
+  <section>
+    <FilmsList />
+  </section>
+</template>
+<style lang="css" scoped>
+
+</style>
