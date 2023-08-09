8d4ddcc674f6090831e3b36075dccce990e02610
ivansheshin <ivashesh@gmail.com>
2023-08-09 18:55:17 +0400

    creadet profile page

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -4,12 +4,55 @@ import {
 } from 'firebase/database'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import {
+  getDownloadURL, getStorage, ref as firebaseRefStore, uploadBytes,
+} from 'firebase/storage'
 import { IUser } from '@/shared/types/user'
 
 const route = useRoute()
 
-const user = ref<IUser>()
+const fileInput = ref()
+const avatar = ref<string>('')
+const imageUrl = ref<string>()
 
+const storage = getStorage()
+const storageRef = firebaseRefStore(storage, `images/${route.params.id}/${avatar.value.name}`)
+
+function setAvatar() {
+  // eslint-disable-next-line prefer-destructuring
+  avatar.value = fileInput.value.files[0]
+}
+
+async function upload() {
+  await uploadBytes(storageRef, avatar.value)
+
+  await getDownloadURL(storageRef)
+    .then((url) => {
+      imageUrl.value = url
+      console.log(imageUrl.value)
+    })
+    .catch((error) => {
+      switch (error.code) {
+        case 'storage/object-not-found':
+          alert('File doesn\'t exist')
+          break
+        case 'storage/unauthorized':
+          alert('User doesn\'t have permission to access the object')
+          break
+        case 'storage/canceled':
+          alert('User canceled the upload')
+          break
+        case 'storage/unknown':
+          alert('Unknown error occurred, inspect the server response')
+          break
+
+        default:
+          break
+      }
+    })
+}
+
+const user = ref<IUser>()
 const dbRef = firebaseRef(getDatabase())
 onMounted(() => {
   get(child(dbRef, `users/${route.params.id}`)).then((snapshot) => {
@@ -24,5 +67,12 @@ onMounted(() => {
     <p v-if="user">
       {{ user.name }}
     </p>
+    <form @submit.prevent="upload">
+      <input ref="fileInput" type="file" @change="setAvatar">
+      <button>
+        Upload
+      </button>
+      <img :src="imageUrl">
+    </form>
   </div>
 </template>
